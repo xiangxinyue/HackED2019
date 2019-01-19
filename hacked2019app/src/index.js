b36682f8f06a146e5281ccb2f9a51ed7f55dd0d9
xiangxinyue <xxiang2@ualberta.ca>
2019-01-19 16:38:34 -0700

    add webpack

--- hacked2019app/src/index.js
+++ hacked2019app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
+import {Tag} from 'antd'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class Home extends React.Component
+{
+    render()
+    {
+        return (
+            <div>
+                <h1>this is the home page</h1>
+                <Tag color="red"><a href="./page1.html">page1</a></Tag>
+                <Tag color="green"><a href="./page2.html">page2</a></Tag>
+            </div> 
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Home/>, document.getElementById('home'));
